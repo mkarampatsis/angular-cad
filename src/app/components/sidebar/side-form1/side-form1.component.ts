c6c770da88f38b189a2eae0d236179b828d9056d
mkarampatsis <marka@central.ntua.gr>
2025-10-23 06:48:00 +0300

    Sidebar Menu

--- src/app/components/sidebar/side-form1/side-form1.component.ts
+++ src/app/components/sidebar/side-form1/side-form1.component.ts
@@ -10,4 +10,21 @@ import { NgbActiveOffcanvas } from '@ng-bootstrap/ng-bootstrap';
 export class SideForm1Component {
   activeOffcanvas = inject(NgbActiveOffcanvas);
 	@Input() name: string | undefined;
+
+  // File actions
+  newFile() { console.log('New File'); }
+  loadFile() { console.log('Load File'); }
+  saveFile() { console.log('Save File'); }
+  importDxf() { console.log('Import DXF'); }
+  exportScene() { console.log('Export Scene'); }
+  publish() { console.log('Publish'); }
+
+  // View actions
+  toggleNodeIDs() { console.log('Toggle Node IDs'); }
+  toggleBeamIDs() { console.log('Toggle Beam IDs'); }
+  toggleColumnIDs() { console.log('Toggle Column IDs'); }
+  toggleGrid() { console.log('Toggle Grid'); }
+
+  // Run
+  runOptimization() { console.log('Run Optimization'); }
 }
